09f1536c27a0a908f5a460cd4d56045a70971856
Uriah Book <812646@mystma.org>
2021-02-20 17:40:51 -0600

    New additions
    added cam controller
    added platform controller
    added player controller
    added additions to the existing floor/track

---
+++ Assets/Scripts/Drag Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//Need to add: 
+//private void OnCollisionEnter2D (Collision2D c)
+//{
+   // if (c.gameObject.tag == "Platform")
+   // {
+   //     transform.SetParent(c.transform);
+   // }
+//}
+
+//private void OnCollisionExit2D(Collision2D c)
+//{
+//    if (c.gameObject.tag == "Platform")
+//    {
+//        transform.SetParent(null);
+//    }
+//}
+//
+//To the player controller script to allow an object/sprite to grab and drag the player.
+//The code bellow allows you to set the start and end position of where you want the object dragging the player to be.
+//Note: Change the tag == "Platform" to a new tag for the object you want to have move the player. Ex: for an elevator you'd first create a tag called "Elevator" 
+//then go to the object you are using as the elevator and change it's tag to the new tag. Finally change the code to:
+//"if (c.gameObject.tag == "Elevator") 
+//{
+//transform.SetParent(c.transform);
+//}
+// Make sure to change Platform in both instances to whatever the new tag is called.
+
+//public class PlatformController : MonoBehaviour
+//{
+
+//    public Vector3 targetPos;
+
+//    public float speed;
+
+//    Vector3 startPos;
+
+//    bool toTarget = true;
+
+    // Start is called before the first frame update
+//    void Start()
+//    {
+//        startPos = transform.position;
+//    }
+//
+    // Update is called once per frame
+//    void Update()
+//    {
+//        if (toTarget == true)
+//        {
+//            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
+//        }
+//
+//        else
+//        {
+//            transform.position = Vector3.MoveTowards(transform.position, startPos, speed);
+//        }
+
+//        if (transform.position == targetPos)
+//        {
+//            toTarget = false;
+//        }
+//
+//        if (transform.position == startPos)
+//        {
+//            toTarget = true;
+//        }
+//    }
+//}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float moveSpeed;
+
+    public float jumpPower;
+
+    Rigidbody2D rb;
+
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpPower);    
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(transform.up * jumpPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+            sr.flipX = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            sr.flipX = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D c)
+    {
+        if (c.gameObject.tag == "Platform")
+        {
+            transform.SetParent(c.transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Platform")
+        {
+            transform.SetParent(null);
+        }
     }
 }
